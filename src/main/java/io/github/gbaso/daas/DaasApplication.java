898ac6ff800f13d76a0284067cbe2f3e17237693
Giacomo Baso <ganondolf@gmail.com>
2023-09-22 19:23:54 +0200

    add project

---
+++ src/main/java/io/github/gbaso/daas/DaasApplication.java
@@ -0,0 +1,13 @@
+package io.github.gbaso.daas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gbaso/daas/DaasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gbaso.daas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
